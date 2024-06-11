0e994bf895f9351bd355fe4874ab1dfed143919b
spinshield <146870398+spinshield@users.noreply.github.com>
2024-06-11 22:35:46 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,7 +12,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:description" content="Documentation for the Games API"" />
+      <meta property="og:description" content="Documentation for the Games API" />
       <meta property="description" content="Documentation for the Games API" />
     </>
   ),
